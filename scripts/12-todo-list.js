e843e011d2e0e5ae90a00653170b2ad1c8dac0cf
nico <vallentenico21@gmail.com>
2025-09-14 15:56:00 +0800

    update

--- scripts/12-todo-list.js
+++ scripts/12-todo-list.js
@@ -11,7 +11,7 @@ renderTodoList();
 function renderTodoList() {
   let todoListHTML = '';
 
-  todoList.forEach(function(todoObject, index) {
+  todoList.forEach((todoObject, index) => {
     const { name, dueDate } = todoObject;
     const html = `
       <div>${name}</div>
@@ -40,7 +40,7 @@ function addTodo() {
     //dueDate: dueDate, - only works if the property and variable name are the same
     name, 
     dueDate
-});
+  });
 
   inputElement.value = '';
 
